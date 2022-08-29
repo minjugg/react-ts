d9cccd2bfdc1678dcf5bdf57f705274feae42d66
minjugee <minjugee211@gmail.com>
2022-08-29 18:48:28 +0900

    🧑‍💻 Lecture 25 ~ 51 react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 // import GuestList from "./state/GuestList";
 // import EventComponent from "./events/EventComponent";
 // import UserSearch from "./state/UserSearch";
-import UserSearch from "./refs/UserSearch";
+// import UserSearch from "./refs/UserSearch";
+import { Provider } from "react-redux";
+import { store } from "./state";
+import RepositoriesList from "./components/RepositoriesList";
 
 function App() {
   return (
-    <div className="App">
-      {/* <GuestList /> */}
-      {/* <EventComponent /> */}
-      <UserSearch />
-    </div>
+    <Provider store={store}>
+      <div>
+        <h1>Search for a package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
   );
 }
 
